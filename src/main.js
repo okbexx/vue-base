95a32adcb978d3627aa82a8d33658be14d16e4e5
Jiahao H <857085930@qq.com>
2018-01-15 15:26:11 +0800

    添加ElementUI-base

--- src/main.js
+++ src/main.js
@@ -4,9 +4,12 @@ import Vue from 'vue'
 import App from './App'
 import router from './core/router'
 import store from './core/store'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
 
 Vue.config.productionTip = false
 Vue.use(router)
+Vue.use(ElementUI)
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
